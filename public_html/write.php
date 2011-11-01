498e71d7b02b21f984db7ea5bd0a307c004651f3
chippen <christoffer.alvenstrand@gmail.com>
2011-11-01 15:54:34 +0100

    skriva nya inlägg sidan Signed-off-by: chippen <christoffer.alvenstrand@gmail.com>

---
+++ public_html/write.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML5>
+<html>
+<head>
+	<title>Scrum Masters bloggen</title>	
+	<meta name="description" content="Scrum masters blogg" />
+	<meta name="author" content="Christoffer, Mario" />
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="wrapper">
+	<header class="header">
+		<h1><a href="index.php">Scrum Masters!</a></h1>
+	</header> <!--här slutar header -->
+	<nav class="nav">
+		<ul>
+			<li><a href="#">Startsida</a></li>
+			<li><a href="#">Skriv anonymt</a></li>
+			<li><a href="#">Logga in/logg ut</a></li>
+		</ul>		
+	</nav> <!--här slutar nax -->
+	<div class="container">
+		<div class="entries">
+			<div class="post-anonym">
+				
+				
+			</div><!-- här slutar post-anonym -->
+		</div><!-- här slutar entries -->
+		<aside class="categories">
+			
+			
+		</aside><!-- här slutar categories -->	
+	</div><!-- här slutar container -->
+</div><!-- här slutar wrapper -->
+<footer class="footer">
+	Design by: ScrumMasters &copy;2011
+</footer><!-- här slutar footer -->
+</body>
+</html>
