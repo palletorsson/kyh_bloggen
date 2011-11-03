c2fa7611a34d198ee50d542f11cb9ccc717821cc
chippen <christoffer.alvenstrand@gmail.com>
2011-11-03 14:34:12 +0100

    bytt plats från include -> script
    Signed-off-by: chippen <christoffer.alvenstrand@gmail.com>

---
+++ public_html/include/script/head.php
@@ -0,0 +1,5 @@
+<title>Scrum Masters bloggen</title>	
+<meta name="description" content="Scrum masters blogg" />
+<meta name="author" content="Christoffer, Mario" />
+<meta charset="utf-8" />
+<link rel="stylesheet" href="css/style.css" type="text/css" />
