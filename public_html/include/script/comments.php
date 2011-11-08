57a061280d2079db02a785d01f930ee584edade4
palle <palle.torsson@gmail.com>
2011-11-08 14:17:07 +0100

    comment function

---
+++ public_html/include/script/comments.php
@@ -0,0 +1,41 @@
+<a href="comments.php?blog_id=1">blogpost 1</a>
+
+<?
+	$blog_post = $_GET["blog_id"];
+	include_once ('dbconnect.php');
+	mysql_select_db("scrummasterdb", $con);
+	$sql="SELECT * FROM comments WHERE blog_id = '$blog_post'";
+	$result = mysql_query($sql, $con) or die(mysql_error());
+
+// 	$row_comment = mysql_fetch_assoc($result);
+	//Här kommer posten att läggas till i databasen om en post är gjort
+	if (isset($_POST["submit"]))
+	{
+		//Här ansluter vi till mysql med ip localhost och användarnamn root, inget lösenord än så länge.
+		mysql_select_db("scrummasterdb", $con);
+		$sql="INSERT INTO comments (author, body) VALUES ('$_POST[author]', '$_POST[body]')";
+		//som kommer att vara blogginläggen.
+		if (!mysql_query($sql,$con))
+		  {
+		  die('Error: ' . mysql_error());
+		  }
+		echo "tack för din kommentar";
+		
+		mysql_close($con);
+	}
+?>
+<fieldset>
+  <legend>Skriv en kommentar:</legend>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" id="commentform">
+Ditt namn <input id="author" name="author" type="text" value="" size="30"/><br />
+Kommentar <textarea id="comment" name="body" cols="45" rows="8" "></textarea><br />
+<input type='hidden' name='comment_post_ID' value="id" id='comment_post_ID' />
+<input name="submit" type="submit" id="submit" value="Kommetera" /> <br />						
+</form> 
+</fieldset>
+Kommentarer: <br />
+<?php	
+	while ($row_comment = mysql_fetch_assoc($result)) { 
+	echo "Kommentar av: ". $row_comment['author'] . "<br />";
+	echo "\" ". $row_comment['body'] . " \"<br /><br />"; 
+} ?> 
