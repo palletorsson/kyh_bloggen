a58188be491329f3b26005e3d4720d563f985e47
palle <palle.torsson@gmail.com>
2011-11-08 10:45:00 +0100

    updated sql repace

--- sql.sql
+++ sql.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 08, 2011 at 10:34 AM
+-- Generation Time: Nov 08, 2011 at 10:43 AM
 -- Server version: 5.1.41
 -- PHP Version: 5.3.2-1ubuntu4.9
 
@@ -42,3 +42,47 @@ CREATE TABLE IF NOT EXISTS `blog_post` (
 INSERT INTO `blog_post` (`id`, `title`, `post`, `idnamn`, `category`, `datum`) VALUES
 (1, '', 'ff', 0, 0, '0000-00-00 00:00:00'),
 (2, '', 'njua', 0, 0, '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `categori` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `categori`) VALUES
+(1, 'Klagomål'),
+(2, 'Beröm'),
+(3, 'Önskemål'),
+(4, 'Snick-snack');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `username` text CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
+  `password` text CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
+  `email` text CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
+  `website` text CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=swe7 COLLATE=swe7_bin AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `website`) VALUES
+(1, 'scrummaster\r\n', '1234test', 'info@scrummaster.com', 'http://www.scrummaster.com');
