4f22427018a73462bbd7a4d300ab90a834f5b538
palle <palle.torsson@gmail.com>
2011-11-08 13:50:45 +0100

    comments sql

--- sql.sql
+++ sql.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 08, 2011 at 10:43 AM
+-- Generation Time: Nov 08, 2011 at 01:48 PM
 -- Server version: 5.1.41
 -- PHP Version: 5.3.2-1ubuntu4.9
 
@@ -18,6 +18,8 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Database: `scrummasterdb`
 --
+CREATE DATABASE `scrummasterdb` DEFAULT CHARACTER SET utf8 COLLATE utf8_swedish_ci;
+USE `scrummasterdb`;
 
 -- --------------------------------------------------------
 
@@ -31,17 +33,16 @@ CREATE TABLE IF NOT EXISTS `blog_post` (
   `post` longtext COLLATE utf8_swedish_ci NOT NULL COMMENT 'post inlägget',
   `idnamn` int(11) NOT NULL,
   `category` int(4) NOT NULL,
-  `datum` datetime NOT NULL,
+  `datum` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `blog_post`
 --
 
 INSERT INTO `blog_post` (`id`, `title`, `post`, `idnamn`, `category`, `datum`) VALUES
-(1, '', 'ff', 0, 0, '0000-00-00 00:00:00'),
-(2, '', 'njua', 0, 0, '0000-00-00 00:00:00');
+(1, '', 'ff', 0, 0, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -68,6 +69,27 @@ INSERT INTO `categories` (`id`, `categori`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `blog_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `author` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `body` text COLLATE utf8_swedish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `blog_id` (`blog_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `comments`
+--
+
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -78,11 +100,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` text CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
   `website` text CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=swe7 COLLATE=swe7_bin AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=swe7 COLLATE=swe7_bin AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `email`, `website`) VALUES
-(1, 'scrummaster\r\n', '1234test', 'info@scrummaster.com', 'http://www.scrummaster.com');
+(0, 'Anonym', '-', '-', '-'),
+(3, 'easy', 'pass', '', ''),
+(4, 'user1', 'pass1', '', ''),
+(5, 'scrummaster', '1234test', '', '');
