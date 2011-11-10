3198fd93a84e1c8793dd961491de8dc260918bc9
palle <palle.torsson@gmail.com>
2011-11-10 11:41:21 +0100

    fix session bug in menu.php

--- public_html/include/script/menu.php
+++ public_html/include/script/menu.php
@@ -1,8 +1,12 @@
 <?php 
+if (isset($_SESSION["username"])) { 
 $sql = "SELECT * FROM user WHERE id = $_SESSION[session_user]";  
 $result = mysql_query($sql, $con) or die(mysql_error());
 $row_name = mysql_fetch_assoc($result); 
 $user = $row_name["username"]; 
+} else {
+$user = ""; 	
+	}
 ?>
 <ul>
 	<li><a href="index.php">Startsida</a></li>
